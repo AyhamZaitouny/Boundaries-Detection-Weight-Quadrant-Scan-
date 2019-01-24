429ef12ee2261e42cce545fea957d212821b4fd3
AyhamZaitouny <44047630+AyhamZaitouny@users.noreply.github.com>
2019-01-24 12:43:57 +0800

    Update quadrant.m

--- quadrant.m
+++ quadrant.m
@@ -1,5 +1,7 @@
 function qq=quadrant(rr)
 
+%This function is to calculate the density quadrant scan curve from a recurrence matrix rr
+
 [dx,dy]=size(rr);
 
 if dx~=dy,
@@ -9,10 +11,10 @@ end;
 hand=waitbar(0,'quadrant');
 
 for i=1:dx
-    pp=rr(1:(i-1),1:(i-1));
-    ff=rr((i+1):dx,(i+1):dx);
-    pf=rr(1:(i-1),(i+1):dx);
-    fp=rr((i+1):dx,1:(i-1));
+    pp=rr(1:(i-1),1:(i-1));  %points at the first quadrant
+    ff=rr((i+1):dx,(i+1):dx);  %points at the third quadrant
+    pf=rr(1:(i-1),(i+1):dx);   %points at the second quadrant
+    fp=rr((i+1):dx,1:(i-1));    % points at the fourth quadrant
     qs=(sum(pp(:))+sum(ff(:))) /((i-1)^2+(dx-i)^2);
     qs=qs / (qs + (sum(pf(:))+sum(fp(:)))/((i-1)*(dx-i)*2));
     qq(i)=qs;
