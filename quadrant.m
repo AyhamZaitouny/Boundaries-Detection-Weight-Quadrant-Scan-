b4ddaa9b13a6415960be078ff9d9cc9be537e448
AyhamZaitouny <44047630+AyhamZaitouny@users.noreply.github.com>
2019-01-30 12:29:50 +0800

    Update quadrant.m

--- quadrant.m
+++ quadrant.m
@@ -11,12 +11,12 @@ end;
 hand=waitbar(0,'quadrant');
 
 for i=1:dx
-    pp=rr(1:(i-1),1:(i-1));  %points at the first quadrant
+    pp=rr(1:i,1:i);  %points at the first quadrant
     ff=rr((i+1):dx,(i+1):dx);  %points at the third quadrant
-    pf=rr(1:(i-1),(i+1):dx);   %points at the second quadrant
-    fp=rr((i+1):dx,1:(i-1));    % points at the fourth quadrant
-    qs=(sum(pp(:))+sum(ff(:))) /((i-1)^2+(dx-i)^2);
-    qs=qs / (qs + (sum(pf(:))+sum(fp(:)))/((i-1)*(dx-i)*2));
+    pf=rr(1:i,(i+1):dx);   %points at the second quadrant
+    fp=rr((i+1):dx,1:i);    % points at the fourth quadrant
+    qs=(sum(pp(:))+sum(ff(:))) /(i^2+(dx-i)^2);
+    qs=qs / (qs + (sum(pf(:))+sum(fp(:)))/(i*(dx-i)*2));
     qq(i)=qs;
     waitbar(i/dx,hand);
 
